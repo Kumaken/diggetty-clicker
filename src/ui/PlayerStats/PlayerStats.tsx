9f72809dce4ade1d6192e9615a54485a1e13949f
Abel Stanley <kumaken132@gmail.com>
2020-08-19 23:05:51 +0700

    encapsulated alerts to separate classes

--- src/ui/PlayerStats/PlayerStats.tsx
+++ src/ui/PlayerStats/PlayerStats.tsx
@@ -10,11 +10,6 @@ import { IconData } from 'phaser/Data/IconData';
 
 const PlayerStats = () => {
 	const store = useContext(RootStoreContext);
-	// const gameScene: Phaser.Scene;
-
-	//   const updateDPC = () => {
-	//     this.DPSText.setElement(StatsText(Player.clickDamage));
-	//   }
 
 	const createDPC = () => {
 		return (
@@ -45,33 +40,6 @@ const PlayerStats = () => {
 			<div className="player-stats-row">{createDPC()}</div>
 		</div>
 	);
-	// this.DPSText = this.gameScene.add.dom(
-	//   AlignTool.getXfromScreenWidth(this.gameScene, 0.12),
-	//   AlignTool.getYfromScreenHeight(
-	//     this.gameScene,
-	//     BottomMenuManager.bottomMenuStartY - 0.175
-	//   ),
-	//   StatsText(Player.clickDamage)
-	// );
-
-	// const img = this.gameScene.add.image(
-	//   AlignTool.getXfromScreenWidth(this.gameScene, 0.05),
-	//   AlignTool.getYfromScreenHeight(
-	//     this.gameScene,
-	//     BottomMenuManager.bottomMenuStartY - 0.181
-	//   ),
-	//   TextureKeys.IC_PICKAXE.key,
-	//   TextureKeys.IC_PICKAXE.frame[0]
-	// );
-	// AlignTool.scaleToScreenWidth(this.gameScene, img, 0.08);
-
-	// listen to game events:
-	// this.gameScene.events.on(GameEvents.OnUpgradeDone, this.updateDPSUI, this);
-
-	//   constructor(scene: Phaser.Scene) {
-	//     this.gameScene = scene;
-	//     this.createDPSUI();
-	//   }
 };
 
 export default observer(PlayerStats);
--- src/ui/UI.tsx
+++ src/ui/UI.tsx
@@ -1,44 +1,20 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import PlayerStats from './PlayerStats/PlayerStats';
 import './UI.css';
 import ToughnessBar from './ToughnessBar/ToughnessBar';
 import { ResourceStats } from './ResourceStats';
 import { BottomMenu } from './BottomMenu/BottomMenu';
-
-import Notification from 'react-bulma-components/lib/components/notification';
-import Button from 'react-bulma-components/lib/components/button';
-import { RootStoreContext } from 'index';
 import { observer } from 'mobx-react';
-import { CSSTransition } from 'react-transition-group';
+import InsufficientMoneyAlert from './Alerts/InsufficientMoney';
 
 const UI = () => {
-	const store = useContext(RootStoreContext);
-
 	return (
 		<div className="UI">
 			<PlayerStats></PlayerStats>
 			<ToughnessBar></ToughnessBar>
 			<ResourceStats></ResourceStats>
 			<BottomMenu></BottomMenu>
-			<CSSTransition
-				in={store.gameStore?.insufficientMoneyNotif}
-				timeout={{ enter: 300, exit: 400 }}
-				classNames="alert"
-			>
-				<>
-					{store.gameStore?.insufficientMoneyNotif && (
-						<Notification color="danger">
-							Insufficient Money!
-							<Button
-								remove
-								onClick={() => {
-									store.gameStore?.setInsufficientMoneyNotif(false);
-								}}
-							/>
-						</Notification>
-					)}
-				</>
-			</CSSTransition>
+			<InsufficientMoneyAlert></InsufficientMoneyAlert>
 		</div>
 	);
 };
